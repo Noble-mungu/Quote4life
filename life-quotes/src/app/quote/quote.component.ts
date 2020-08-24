9dff3af65183766c1af8c9e3321df1665d96e31c
Noble Mungu <mungunoble@gmail.com>
2020-08-24 18:33:38 +0300

    add quotes

--- life-quotes/src/app/quote/quote.component.ts
+++ life-quotes/src/app/quote/quote.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
+import { Form } from '@angular/forms';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-	quotes = [
-	new Quote("Inauma itabidi uzoee","Simple Boy","SWahili",0,0,new Date(2013,5,5)),
-	new Quote("Ooliskia wapi","Ule Msee","Says",0,0,new Date(2010,4,3)),
-	new Quote("Hardwork pays","English","Proverb",0,0, new Date(2015,6,6)),
-	new Quote("Yaliyopita si ndewle","Swahili","Proverb",new Date(2017,3,2)),
+  quotes = [
+   new Quote("Hardwork pays", "English", "Proverb", 0, 0, new Date(2010,6,13)),
+    new Quote("Mtaka cha mvunguni sharti ainame", "Ken", "Walibora", 0, 0, new Date(2019,6,4)),
+    new Quote("Tout est beau tant que tu l'impose", "Willy William", "Mozzy", 0, 0, new Date(2012,7,21)),
+    new Quote("Yaliyopita si ndwele", "KIswahili", "Proverb", 0, 0, new Date(2019,8,10))
+  ];
 
-	]
 
   constructor() {}
 
@@ -32,12 +34,11 @@ export class QuoteComponent implements OnInit {
 
   submitType = 'save';
 
-   quote = [
-   new Quote("Inauma itabidi uzoee","Simple Boy","SWahili",0,0,new Date(2013,5,5)),
-	new Quote("Ooliskia wapi","Ule Msee","Says",0,0,new Date(2010,4,3)),
-	new Quote("Hardwork pays","English","Proverb",0,0, new Date(2015,6,6)),
-	new Quote("Yaliyopita si ndewle","Swahili","Proverb",new Date(2017,3,2)),
-
+  quote = [
+    new Quote("Hardwork pays", "English", "Proverb", 0, 0, new Date(2010,6,13)),
+    new Quote("Mtaka cha mvunguni sharti ainame", "Ken", "Walibora", 0, 0, new Date(2019,6,4)),
+    new Quote("Tout est beau tant que tu l'impose", "Willy William", "Mozzy", 0, 0, new Date(2012,7,21)),
+    new Quote("Yaliyopita si ndwele", "KIswahili", "Proverb", 0, 0, new Date(2019,8,10))
   ];
 
   deleteQuote(isComplete, index){
