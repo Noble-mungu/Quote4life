1156d5381d965df65bcaf8509998b22e63aec19d
Noble Mungu <mungunoble@gmail.com>
2020-08-25 08:23:57 +0300

    add forms module

--- life-quotes/src/app/app.module.ts
+++ life-quotes/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
@@ -12,9 +12,12 @@ import { FormComponent } from './form/form.component';
     QuoteComponent,
     QuoteDetailComponent,
     FormComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
