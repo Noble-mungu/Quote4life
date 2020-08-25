95fe90fe9489ec7094272debd04d784dc6708647
Noble Mungu <mungunoble@gmail.com>
2020-08-25 15:37:54 +0300

    update file

--- life-quotes/src/app/app.module.ts
+++ life-quotes/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
--- life-quotes/src/app/quote/quote.component.ts
+++ life-quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-thisimport { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 import { Form } from '@angular/forms';
 
